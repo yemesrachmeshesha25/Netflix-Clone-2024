90e65626ff649c7e38ef1b0a7e76c9581beec510
yemesrachmeshesha25 <154487046+yemesrachmeshesha25@users.noreply.github.com>
2024-01-25 12:03:48 +0300

    row

--- netflix-clone/src/components/Header/Header.js
+++ netflix-clone/src/components/Header/Header.js
@@ -25,6 +25,7 @@ const Header = () => {
 <div className='header_right'>
   <ul>
     <li><SearchIcon /></li>
+    <li>kids</li>
     <li><NotificationsNoneIcon /></li>
     <li><AccountBoxIcon /></li>
     <li><ArrowDropDownIcon /></li>
