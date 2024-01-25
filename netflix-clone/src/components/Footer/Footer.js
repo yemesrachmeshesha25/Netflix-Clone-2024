55b3da72fd01cbab266ccd50ed1bc7581cf29ff8
yemesrachmeshesha25 <154487046+yemesrachmeshesha25@users.noreply.github.com>
2024-01-25 10:18:39 +0300

    row

--- netflix-clone/src/components/Footer/Footer.js
+++ netflix-clone/src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import XIcon from '@mui/icons-material/X';
 import "./footer.css"
 
 const Footer = () => {
@@ -12,46 +13,47 @@ const Footer = () => {
           <FacebookOutlinedIcon />
           <InstagramIcon />
           <YouTubeIcon />
-      </div>
-       <div className='footer_data'>
-        <div>
-          <ul>
-            <li>Audio Description</li>
-            <li>Investor Relations</li>
-            <li>Legal Notice</li>
-          </ul>
+          <XIcon/>
         </div>
-        <div>
-          <ul>
-            <li>Help Center</li>
-            <li>Jobs</li>
-            <li>Cookie Preferences</li>
-          </ul>
+        <div className='footer_data'>
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <ul>
-            <li>Gift Cards</li>
-            <li>Terms of Use</li>
-            <li>Corporate Information</li>
-          </ul>
+        <div className='service_code'>
+          <p>
+            Service Code
+          </p>
         </div>
-        <div>
-          <ul>
-            <li>Media Center</li>
-            <li>Privacy</li>
-            <li>Contact Us</li>
-          </ul>
+        <div className='copy-write'>
+          &copy; 1997-2024 Netflix, Inc.
         </div>
       </div>
-      <div className='service_code'>
-        <p>
-          Service Code
-        </p>
-      </div>
-      </div>
-      <div className='copy-write'>
-        &copy;1997-2024 Netflix,Inc.
-    </div>
     </div>
   )
 }
